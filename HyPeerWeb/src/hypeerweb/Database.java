653d98089b00a9889260e550e428d51421f7dd99
gwerner <gwwerner@hotmail.com>
2013-10-02 10:48:19 -0600

    Made a change to HyPeerWebTest.java so that it would test recovery from database

--- HyPeerWeb/src/hypeerweb/Database.java
+++ HyPeerWeb/src/hypeerweb/Database.java
@@ -452,9 +452,6 @@ public final class Database {
 		List<Integer> neighbors = new ArrayList<>();
 		while (set.next())
 			neighbors.add(set.getInt(1));
-                set = sqlQuery("SELECT WebId FROM Neighbors WHERE Neighbor = "+webid+";");
-                while (set.next())
-			neighbors.add(set.getInt(1));
 		return neighbors;
 	}
 	/**
--- HyPeerWeb/test/hypeerweb/HyPeerWebTest.java
+++ HyPeerWeb/test/hypeerweb/HyPeerWebTest.java
@@ -21,32 +21,42 @@ public class HyPeerWebTest {
 	public HyPeerWebTest() throws Exception{
 		web = HyPeerWeb.getInstance();
 	}
-	
+        
+	/*
 	@Test
 	public void testHyPeerWeb(){
 		//Validate the web prior to adding to make sure
 		//it is extracting from the Database correctly
+                System.out.println("Testing restore");
 		assertTrue((new Validator(web)).validate());
+                System.out.println("Done testing restore");
 	}
-	
+	*/
+        
 	/**
 	 * Test of addNode method, of class HyPeerWeb.
 	 */
 	@Test
 	public void testAddNode() throws Exception {
 		HyPeerWeb web = HyPeerWeb.getInstance();
-		web.deleteAllNodes();
 		Node n;
+                //I put the testHyPeerWeb code here because it was always running after testAddNode as so wasn't testing anything.
+                System.out.println("Testing restore");
+		assertTrue((new Validator(web)).validate());//comment out this line to get new HyPeerWeb
+                System.out.println("Done testing restore");
+                
 		//Add a bunch of nodes; if it validates afterwards, addNode should be working
 		//We cannot do simulated tests, since addNode inserts at arbitrary places
+                web.deleteAllNodes();
 		boolean valid;
-		for (int i=0; i<MAX_TESTS; i++){
+		for (int i=1; i<=MAX_TESTS; i++){
 			n = web.addNode();
+                        System.out.println("Added node #" + n.getWebId());
 			if (i % TEST_EVERY == 0){
 				valid = (new Validator(web)).validate();
 				assertTrue(valid);
 			}
-			System.out.println("Added node #" + n.getWebId());
+			
 		}
 		
 	}
