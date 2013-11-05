5b5b639e2f0cf5c52dab9a333f18f57643df4f9c
Azmisov <ozixtheorange@gmail.com>
2013-11-04 18:49:27 -0700

    setup for testing jamie's group's code

---
+++ HyPeerWeb/test/jamie/JamieHyPeerWebTest.java
@@ -0,0 +1,14 @@
+package jamie;
+
+import jamie.model.HyperWeb;
+
+/**
+ * Test cases for Jamie's group's HyPeerWeb
+ * @author isaac
+ */
+public class JamieHyPeerWebTest {
+	
+	public JamieHyPeerWebTest() throws Exception{
+		HyperWeb x = new HyperWeb();
+	}
+}
